9545d4bd3119215819b2756c9403777583dba7a7
Huynh Buu Tam <o0johntam0o@gmail.com>
2015-02-26 09:41:01 +0700

    Correct Polish translation

--- ext/o0johntam0o/quickads/language/pl/quick_ads.php
+++ ext/o0johntam0o/quickads/language/pl/quick_ads.php
@@ -1,7 +1,8 @@
 <?php
 /**
 *
-* Quick Ads extension for the phpBB Forum Software package [British English]
+* Quick Ads extension for the phpBB Forum Software package [Polish]
+* Polish translation by Hapek
 *
 * @copyright (c) 2014 o0johntam0o
 * @license GNU General Public License, version 2 (GPL-2.0)
@@ -28,17 +29,19 @@ $lang = array_merge($lang, array(
 	
 	'QUICK_ADS_ENABLE'					=> 'Włącz reklamy:',
 	'QUICK_ADS_ENABLE_EXPLAIN'			=> 'Jeżeli nie włączysz tej opcji, reklamy nie będą wyświetlane.',
-	'QUICK_ADS_ALLOW_BOT'				=> 'Pokaż botom:',
+	'QUICK_ADS_ALLOW_BOT'				=> 'Pokaż botom',
 	'QUICK_ADS_ALLOW_BOT_EXPLAIN'		=> 'Wyświetla reklamy botom.',
-	'QUICK_ADS_CUSTOM_ID'				=> 'Własne ID:',
+	'QUICK_ADS_CUSTOM_ID'				=> 'Własne ID',
 	'QUICK_ADS_CUSTOM_ID_EXPLAIN'		=> 'Używaj wyłącznie liter <em style="color:#ff0000">a-z</em>, cyfr <em style="color:#ff0000">0-9</em> oraz <em style="color:#ff0000">podkreślenia</em> w miejsce odstępu (Zostaw puste, by użyć domyślnej wartości). Przykład: my_custom_id_123',
-	'QUICK_ADS_ZINDEX'					=> 'Z-Index (wartości całkowite):',
+	'QUICK_ADS_ZINDEX'					=> 'Z-Index (wartości całkowite)',
 	'QUICK_ADS_ZINDEX_EXPLAIN'			=> 'Umieszczaj reklamy "za" lub "przed" innymi elementami.',
-	'QUICK_ADS_CLOSEBT'					=> 'Pokaż przycisk zamykania:',
-	'QUICK_ADS_CLOSEBT_EXPLAIN'			=> 'By użytkownik mógł zamykać wyświetlane reklamy.',
-	'QUICK_ADS_COOKIE'					=> 'Włącz ciasteczka:',
+	'QUICK_ADS_CLOSEBT'					=> 'Show close button',
+	'QUICK_ADS_CLOSEBT_EXPLAIN'			=> 'To close Ads',
+	'QUICK_ADS_CLOSEBT_FRAME'			=> 'On each frame',
+	'QUICK_ADS_CLOSEBT_ADS'				=> 'On each Ads',
+	'QUICK_ADS_COOKIE'					=> 'Włącz ciasteczka',
 	'QUICK_ADS_COOKIE_EXPLAIN'			=> 'Aby reklamy były wyświetlane poprawnie.',
-	'QUICK_ADS_COOKIE_TIME'				=> 'Czas ciasteczek (wartości całkowite):',
+	'QUICK_ADS_COOKIE_TIME'				=> 'Czas ciasteczek (wartości całkowite)',
 	'QUICK_ADS_COOKIE_TIME_EXPLAIN'		=> 'Jak długo reklama będzie ukryta po jej zamknięciu (w minutach).',
 
 	'QUICK_ADS_LEGEND'					=> '%s [%d]',
@@ -46,14 +49,14 @@ $lang = array_merge($lang, array(
 	'QUICK_ADS_MORE_PROP'				=> '<em style="color:#0000ff">Zaawansowane (kliknij by rozwinąć)</em>',
 	'QUICK_ADS_NAME'					=> 'Nazwa reklamy:',
 	'QUICK_ADS_NAME_EXPLAIN'			=> 'Możesz zmienić nazwę w dowolnej chwili.',
-	'QUICK_ADS_POS'						=> 'Pozycja:',
+	'QUICK_ADS_POS'						=> 'Pozycja',
 	'QUICK_ADS_POS_EXPLAIN'				=> 'Ustaw pozycję reklamy.<br />Wybierz "Zablokuj" by wyłączyć wyświetlanie reklamy.',
 	'QUICK_ADS_TOP'						=> 'reklama u góry',
 	'QUICK_ADS_LEFT'					=> 'reklama po lewej',
 	'QUICK_ADS_BOTTOM'					=> 'reklama na dole',
 	'QUICK_ADS_RIGHT'					=> 'reklama po prawej',
 	
-	'QUICK_ADS_ONPAGE'					=> 'Pokaż reklamę na:',
+	'QUICK_ADS_ONPAGE'					=> 'Pokaż reklamę na',
 	'QUICK_ADS_ONPAGE_EXPLAIN'			=> 'Na której stronie ma wyświetlać się reklama?',
 	'QUICK_ADS_ONPAGE_ITEM_FAQ'			=> 'Najczęściej zadawane pytania (FAQ)',
 	'QUICK_ADS_ONPAGE_ITEM_INDEX'		=> 'Główna',
@@ -70,14 +73,14 @@ $lang = array_merge($lang, array(
 	// 'QUICK_ADS_ONPAGE_ITEM_YOUR_STUFF'	=> 'Your stuff',
 	// Custom pages
 	
-	'QUICK_ADS_GROUP'					=> 'Pokaż reklamę grupom:',
+	'QUICK_ADS_GROUP'					=> 'Pokaż reklamę grupom',
 	'QUICK_ADS_GROUP_EXPLAIN'			=> 'Której grupie ma wyświetlać się reklama?',
-	'QUICK_ADS_HREF'					=> 'Link:',
+	'QUICK_ADS_HREF'					=> 'Link',
 	'QUICK_ADS_HREF_EXPLAIN'			=> 'Podaj link do strony, do której odeśle użytkownika po kliknięciu w reklamę.',
 	
-	'QUICK_ADS_BG_IMG'					=> 'Grafika w tle:',
-	'QUICK_ADS_BG_IMG_EXPLAIN'			=> 'Link do obrazka (www).',
-	'QUICK_ADS_BG_COLOR'				=> 'Kolor tła:',
+	'QUICK_ADS_BG_IMG'					=> 'Grafika w tle',
+	'QUICK_ADS_BG_IMG_EXPLAIN'			=> 'Link do obrazka (www)',
+	'QUICK_ADS_BG_COLOR'				=> 'Kolor tła',
 	'QUICK_ADS_BG_COLOR_EXPLAIN'		=> 'Przykład: #ffffff, #000000, white, black,...',
 	
 	'QUICK_ADS_TEXT'					=> 'Treść reklamy',
@@ -94,7 +97,7 @@ $lang = array_merge($lang, array(
 	'QUICK_ADS_SIZE_EXPLAIN'			=> 'Ustaw rozmiar reklamy - (szerokość x wysokość) w pikselach. Zero (0) ustawia rozmiar automatycznie.',
 	'QUICK_ADS_MSIZE'					=> 'Minimalny rozmiar (wartości całkowite)',
 	'QUICK_ADS_MSIZE_EXPLAIN'			=> 'Ustaw minimalny rozmiar okna przeglądarki, od którego wyświetlać reklamy - (szerokość x wysokość) w pikselach.',
-	'QUICK_ADS_OVERF'					=> 'Przepełnienie:',
+	'QUICK_ADS_OVERF'					=> 'Przepełnienie',
 	'QUICK_ADS_OVERF_EXPLAIN'			=> 'Ustaw co się stanie, gdy element nie mieści się w podanym przez Ciebie rozmiarze.',
 	'QUICK_ADS_OVERF_HIDDEN'			=> 'Ukryj nadmiar',
 	'QUICK_ADS_OVERF_VISIBLE'			=> 'Pokazuj całość',
@@ -105,8 +108,9 @@ $lang = array_merge($lang, array(
 
 	'QUICK_ADS_NEW_ADS'					=> 'Nowa reklama',
 	'QUICK_ADS_NEW_ADS_NAME'			=> 'Reklama',
-	'QUICK_ADS_DEL_FIELD'				=> 'Usuń reklamę',
-	'QUICK_ADS_DEL_FIELD_EXPLAIN'		=> '<strong style="color:#ff0000">Uwaga: tej akcji nie można cofnąć</strong>.',
+	'QUICK_ADS_DEL_ADS'					=> '<strong>Delete this Ads</strong>',
+	'QUICK_ADS_DEL_ADS_CONFIRM'			=> 'Do you really want to delete this Ads?<br/><strong style="color:#ff0000">Note: this action can not be undo</strong>',
+	'QUICK_ADS_DEL_ADS_DELETED'			=> 'Deleted!',
 	
 	'QUICK_ADS_SAVED'					=> 'Konfiguracja została zmieniona.',
 	'QUICK_ADS_LOG_MSG'					=> '<strong>Zmienione ustawienia systemu reklam</strong>',
